27ce40dcc34c5d3b595b503aae144dd2b182e188
Pablo Rachid de Bem <rachidpablodebem@gmail.com>
2025-08-12 04:22:52 -0300

    criar base do jogo no documento + inicio da configuraçao visual

---
+++ jogador.js
@@ -0,0 +1,29 @@
+export function criarJogador()
+{
+    const fazendeiro = 
+    {
+        nome: "Pablo",
+        //controles possíveis: "arar", "plantar", "regar", "colher", "arrancar",
+        // em caso de semente, cenoura, trigo ou café 
+        controle: 
+        {
+            acao: null,
+            semente: null,
+        },
+        dinheiro: 100,
+        silo: 
+        {
+            trigo: 0,
+            cenoura: 0,
+            café: 0
+        },
+        sementes:
+         {
+            trigo: 5,
+            cenoura: 5,
+            café: 5
+        },
+        semana: 0
+    };
+    return jogador;
+}
--- jogo.js
+++ jogo.js
@@ -3,9 +3,27 @@ import { criarJogador } from './jogador.js';
 //import { clicaSlot } from './clicaSlot.js';
 //...........................................................................................
 
-iniciaJogo();
+const jogo = document.querySelector('#jogo');
 
-function iniciaJogo() {
-    const terreno = criaTerreno();
-    const jogador = criarJogador();
+base(jogo);
+//iniciaJogo();
+
+/*
+function iniciaJogo()
+{
+    const terreno1 = criaTerreno();
+    const jogador2 = criarJogador();
+
 }
+*/
+function base(jogo)
+{
+    const fundoTerreno = document.createElement('div');
+    fundoTerreno.classList.add('terreno');
+    const fundoBarraAtividades = document.createElement('div');
+    fundoBarraAtividades.classList.add('barraAtividades');
+    jogo.append(fundoTerreno);
+    jogo.append(fundoBarraAtividades); 
+}
+
+//function desenhaTerreno()
